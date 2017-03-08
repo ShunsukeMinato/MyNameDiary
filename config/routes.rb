5e4bf444fa06a19f2a515bc27ea8143c96babf90
ShunsukeMinato <kururmiruten@gmail.com>
2017-03-08 10:53:09 +0000

    練習

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "articles/new"
+  #"articles/new" => "articles#new"を省略できる。
   
   post "/articles" => "articles#create"
-
+  
 end
